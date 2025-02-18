f96802e45b1cf166f605133a567293f2cb3684fd
JHONPLIQUIS <126591413+JHONPLIQUIS@users.noreply.github.com>
2025-02-18 10:28:25 -0300

    Add files via upload

--- app.py/app.py
+++ app.py/app.py
@@ -1,12 +1,20 @@
-print('neguin sabores\n')
+import os
 
-print('1. cadastrar restaurante')
-print('2. listar restaurante')
-print('3. ativar restaurante')
-print('4. sair')
+def exibir_nome_do_programa():
+    print('neguin sabores\n')
+
+def exibir_opcoes():
+    print('1. cadastrar restaurante')
+    print('2. listar restaurante')
+    print('3. ativar restaurante')
+    print('4. sair')
 
 opcao_escolhida = int(input('escolha uma opção: '))
 print(f'você escolheu a opção{opcao_escolhida}\n')
+
+def finalizar_app():
+     os.system('cls')
+     print('finalizar o app')
 
 if opcao_escolhida == 1:
     print('cadastrar restaurante')
@@ -15,5 +23,12 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida ==3:
         print('ativar restaurante')
 else:
-        print('encerrando o programa')
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+
+if __name__ == '__main__':
+   main()
 
